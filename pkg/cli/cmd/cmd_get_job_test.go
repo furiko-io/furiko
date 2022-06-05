c4f761faa7c23ef1c2563dac09dd72e7fdbdd215
Irvin Lim <irvinlimweiquan@gmail.com>
2022-06-05 20:52:58 +0800

    feat(cli): Add more unit tests

--- pkg/cli/cmd/cmd_get_job_test.go
+++ pkg/cli/cmd/cmd_get_job_test.go
@@ -17,15 +17,18 @@
 package cmd_test
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/pointer"
 
 	execution "github.com/furiko-io/furiko/apis/execution/v1alpha1"
 	"github.com/furiko-io/furiko/pkg/cli/cmd"
 	"github.com/furiko-io/furiko/pkg/cli/formatter"
+	"github.com/furiko-io/furiko/pkg/execution/util/parallel"
 	runtimetesting "github.com/furiko-io/furiko/pkg/runtime/testing"
 	"github.com/furiko-io/furiko/pkg/utils/testutils"
 )
@@ -117,6 +120,89 @@ var (
 			},
 		},
 	}
+
+	jobParallel = &execution.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "job-parallel",
+			Namespace: DefaultNamespace,
+		},
+		Spec: execution.JobSpec{
+			Template: &execution.JobTemplate{
+				Parallelism: &execution.ParallelismSpec{
+					WithCount:          pointer.Int64(3),
+					CompletionStrategy: execution.AllSuccessful,
+				},
+			},
+		},
+		Status: execution.JobStatus{
+			Phase: execution.JobRunning,
+			Condition: execution.JobCondition{
+				Running: &execution.JobConditionRunning{
+					LatestCreationTimestamp: testutils.Mkmtime(taskCreateTime),
+					LatestRunningTimestamp:  testutils.Mkmtime(taskLaunchTime),
+				},
+			},
+			StartTime:    testutils.Mkmtimep(startTime),
+			CreatedTasks: 3,
+			ParallelStatus: &execution.ParallelStatus{
+				ParallelStatusSummary: execution.ParallelStatusSummary{
+					Complete: false,
+				},
+				Indexes: []execution.ParallelIndexStatus{
+					{
+						Index: execution.ParallelIndex{
+							IndexNumber: pointer.Int64(0),
+						},
+						Hash:         makeHash(0),
+						CreatedTasks: 1,
+						State:        execution.IndexRunning,
+					},
+					{
+						Index: execution.ParallelIndex{
+							IndexNumber: pointer.Int64(1),
+						},
+						Hash:         makeHash(1),
+						CreatedTasks: 1,
+						State:        execution.IndexRunning,
+					},
+					{
+						Index: execution.ParallelIndex{
+							IndexNumber: pointer.Int64(2),
+						},
+						Hash:         makeHash(2),
+						CreatedTasks: 1,
+						State:        execution.IndexRunning,
+					},
+				},
+			},
+			Tasks: []execution.TaskRef{
+				{
+					Name:              "job-parallel-0",
+					CreationTimestamp: testutils.Mkmtime(taskCreateTime),
+					RunningTimestamp:  testutils.Mkmtimep(taskLaunchTime),
+					Status: execution.TaskStatus{
+						State: execution.TaskRunning,
+					},
+				},
+				{
+					Name:              "job-parallel-0",
+					CreationTimestamp: testutils.Mkmtime(taskCreateTime),
+					RunningTimestamp:  testutils.Mkmtimep(taskLaunchTime),
+					Status: execution.TaskStatus{
+						State: execution.TaskRunning,
+					},
+				},
+				{
+					Name:              "job-parallel-0",
+					CreationTimestamp: testutils.Mkmtime(taskCreateTime),
+					RunningTimestamp:  testutils.Mkmtimep(taskLaunchTime),
+					Status: execution.TaskStatus{
+						State: execution.TaskRunning,
+					},
+				},
+			},
+		},
+	}
 )
 
 func TestGetJobCommand(t *testing.T) {
@@ -181,9 +267,57 @@ func TestGetJobCommand(t *testing.T) {
 			},
 		},
 		{
+			Name:     "get a finished job, pretty print with detail",
+			Args:     []string{"get", "job", "job-finished", "-o", "detail"},
+			Fixtures: []runtime.Object{jobFinished},
+			Stdout: runtimetesting.Output{
+				// Should print list of tasks and their details.
+				ContainsAll: []string{
+					jobFinished.Status.Tasks[0].Name,
+					jobFinished.Status.Tasks[0].Status.Message,
+				},
+			},
+		},
+		{
+			Name:     "get a parallel job",
+			Args:     []string{"get", "job", "job-parallel"},
+			Fixtures: []runtime.Object{jobParallel},
+			Stdout: runtimetesting.Output{
+				// Should print parallel task summary.
+				ContainsAll: []string{
+					string(jobParallel.Spec.Template.Parallelism.GetCompletionStrategy()),
+					"3 Running",
+				},
+			},
+		},
+		{
+			Name:     "get a parallel job with detail",
+			Args:     []string{"get", "job", "job-parallel", "-o", "detail"},
+			Fixtures: []runtime.Object{jobParallel},
+			Stdout: runtimetesting.Output{
+				// Should print parallel task groups.
+				ContainsAll: []string{
+					makeHash(0),
+				},
+				MatchesAll: []*regexp.Regexp{
+					regexp.MustCompile(`Index Number:\s+0`),
+				},
+			},
+		},
+		{
 			Name:      "get job does not exist",
 			Args:      []string{"get", "job", "job-running"},
 			WantError: runtimetesting.AssertErrorIsNotFound(),
 		},
 	})
+}
+
+func makeHash(index int64) string {
+	hash, err := parallel.HashIndex(execution.ParallelIndex{
+		IndexNumber: pointer.Int64(index),
+	})
+	if err != nil {
+		panic(err)
+	}
+	return hash
 }
