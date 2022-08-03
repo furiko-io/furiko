0a3e14a9dcd888e60a734ae435f2d9fa15136af8
Irvin Lim <irvinlimweiquan@gmail.com>
2022-08-04 04:40:37 +0800

    chore(yaml): Revert change to events RBAC

--- cmd/execution-controller/main.go
+++ cmd/execution-controller/main.go
@@ -37,8 +37,7 @@ import (
 	"github.com/furiko-io/furiko/pkg/utils/yaml"
 )
 
-// +kubebuilder:rbac:groups="",resources=events,verbs=create
-// +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=events;pods,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;watch
 // +kubebuilder:rbac:groups=execution.furiko.io,resources=jobs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=execution.furiko.io,resources=jobs/status,verbs=get;update;patch
