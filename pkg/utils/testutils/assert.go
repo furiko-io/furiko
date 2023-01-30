c11a29ad72823bb033f705e8bee00f36ca797747
Irvin Lim <irvinlimweiquan@gmail.com>
2023-01-31 01:55:35 +0800

    feat(cli): Implement logs command

--- pkg/utils/testutils/assert.go
+++ pkg/utils/testutils/assert.go
@@ -37,6 +37,14 @@ func AssertErrorIsNotFound() assert.ErrorAssertionFunc {
 	}
 }
 
+// AssertErrorContains returns assert.ErrorAssertionFunc that asserts that the error
+// message contains str.
+func AssertErrorContains(str string) assert.ErrorAssertionFunc {
+	return func(t assert.TestingT, err error, i ...interface{}) bool {
+		return assert.Contains(t, err.Error(), str, i...)
+	}
+}
+
 // WantError checks err against assert.ErrorAssertionFunc, returning true if an
 // error was encountered for short-circuiting.
 func WantError(t assert.TestingT, wantErr assert.ErrorAssertionFunc, err error, i ...interface{}) bool {
